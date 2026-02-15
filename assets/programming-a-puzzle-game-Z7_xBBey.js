1bcf5408c9903aca60ba5c31267248e75e80d384
CI <CI@example.com>
2026-02-15 12:57:15 +0000

    deploy

---
+++ assets/programming-a-puzzle-game-Z7_xBBey.js
@@ -0,0 +1,8 @@
+import{d as o,r as n,a as r,c as l,w as s,A as u,o as d,b as e,e as t}from"./index-BEoJ59tX.js";const m=o({__name:"programming-a-puzzle-game",setup(c){const i=n(r["programming-a-puzzle-game"]);return(g,a)=>(d(),l(u,{article:i.value},{default:s(()=>[...a[0]||(a[0]=[e("p",null,`
+            My oldest son, age 8, has for a while been asking about building a game. Initial ambitions started at the level of Mario Kart, but I ultimately negotiated it down to a puzzle game for which he could provide his own art.
+        `,-1),t(),e("p",null,`
+            He helped design the interface, its logic, difficulty levels, and suggested the timer functionality. With a few helpful flexbox assists from GitHub CoPilot, we managed to build it on a single Sunday morning. His little brother contributed another piece of art and
+            eagerly play-tested the game after. Grandma tried it too, so it has been approved for ages 5 to 70.
+        `,-1),t(),e("p",null,[t(`
+            You can try it out by clicking `),e("a",{href:"/puzzle-game"},"here"),t(`.
+        `)],-1)])]),_:1},8,["article"]))}});export{m as default};
